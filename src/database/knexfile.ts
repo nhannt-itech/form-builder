016f17c6ab2cb1b93ee97b2665a6349baf083aec
Nguyen Thanh Nhan <nhannt.itech@gmail.com>
2022-01-08 20:40:14 +0700

    initial project

---
+++ src/database/knexfile.ts
@@ -0,0 +1,19 @@
+const knexfile = {
+	development: {
+		client: "postgresql",
+		connection: {
+			database: "formbuilder_db",
+			user: "postgres",
+			password: "1qaz!QAZ",
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
+};
+
+export default knexfile;
