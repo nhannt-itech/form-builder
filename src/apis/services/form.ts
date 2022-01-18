fbe3c13870a3b7d701a0c9959c38343ad499da94
Nguyen Thanh Nhan <nhannt.itech@gmail.com>
2022-01-18 22:59:17 +0700

    update: fix service api with transaction.

--- src/apis/services/form.ts
+++ src/apis/services/form.ts
@@ -6,14 +6,17 @@ class formService {
 	async save(formRequest: FormRequest): Promise<String> {
 		const { name, description, formElements } = formRequest;
 		const formId = uuidv4();
-		await db("Forms").insert({ formId, name, description }).returning<String>("formId");
-		await db("FormVersions").insert({
-			formVersionId: uuidv4(),
-			formId,
-			versionNo: "1",
-			formElements,
+
+		return await db.transaction(async (trx) => {
+			await trx("Forms").insert({ formId, name, description }, "formId");
+			await trx("FormVersions").insert({
+				formVersionId: uuidv4(),
+				formId,
+				versionNo: "1",
+				formElements,
+			});
+			return formId;
 		});
-		return formId;
 	}
 
 	async list(formListRequest: FormListRequest): Promise<FormListResponse> {
@@ -23,21 +26,17 @@ class formService {
 			.count("name", "like", search + "% as totalPage")
 			.whereIn("status", status)
 			.first()) as any;
-
 		const nextPage = page * pageSize < totalPage;
+		const lastFormVersions = db("FormVersions")
+			.select("formId")
+			.max({ updatedAt: "createdAt" })
+			.groupBy("formId")
+			.as("FormVersions");
 		const forms = await db("Forms")
 			.select<Array<FormObject>>("name", "status", "description", "updatedAt")
 			.where("name", "like", search + "%")
 			.whereIn("status", status)
-			.join(
-				db("FormVersions")
-					.select("formId")
-					.max({ updatedAt: "createdAt" })
-					.groupBy("formId")
-					.as("FormVersions"),
-				"FormVersions.formId",
-				"Forms.formId"
-			)
+			.join(lastFormVersions, "FormVersions.formId", "Forms.formId")
 			.limit(pageSize)
 			.offset(offset);
 
